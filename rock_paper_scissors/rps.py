2522c6dca8d320e12ed5216bab00a16e2293271f
Heorhii Siburov <g.siburov@gmail.com>
2020-01-22 23:56:28 -0800

    some results with rps task

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,26 @@
 import sys
 
 def rock_paper_scissors(n):
+  add = ['rock', 'paper', 'scissors']
+  result = []
+  if n == 0:
+    result = [[]]
+  if n == 1:
+    for i in range(0, 3):
+      result.append([add[i]])
+  if n > 1:
+    for i in range(0, 3**n):
+      result.append(add[2])
+
+  print("Result is:", result)
   pass 
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+rock_paper_scissors(1)
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
